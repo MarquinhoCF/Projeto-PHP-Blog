101358635b32029995a5567da31d5bd8019002eb
MarquinhoCF <marcos13@gmail.com>
2024-03-05 10:50:36 -0300

    Finalizando a home

--- index.php
+++ index.php
@@ -7,25 +7,25 @@
             <h1>Code Blog</h1>
             <p>O seu blog de programação</p>
         </div>
-    </main>
+    
 
-    <div id="posts">
-        <?php foreach($posts as $post): ?>
-            <div id="post-box">
-                <img src="<?= $BASE_URL ?>/img/<?= $post['img'] ?>" alt="<?= $post['title'] ?>">
-                <h2 id="post-title">
-                    <a href="<?= $BASE_URL ?>post.php?id=<?= $post['id'] ?>"><?= $post['title'] ?></a>
-                </h2>
-                <p class="post-description"><?= $post['description'] ?></p>
-                <div class="tags-container">
-                    <?php foreach($post['tags'] as $tag): ?>
-                        <a href="#"><?= $tag ?></a>
-                    <?php endforeach ?>
+        <div id="posts-container">
+            <?php foreach($posts as $post): ?>
+                <div class="post-box">
+                    <img src="<?= $BASE_URL ?>/img/<?= $post['img'] ?>" alt="<?= $post['title'] ?>">
+                    <h2 class="post-title">
+                        <a href="<?= $BASE_URL ?>post.php?id=<?= $post['id'] ?>"><?= $post['title'] ?></a>
+                    </h2>
+                    <p class="post-description"><?= $post['description'] ?></p>
+                    <div class="tags-container">
+                        <?php foreach($post['tags'] as $tag): ?>
+                            <a href="#"><?= $tag ?></a>
+                        <?php endforeach ?>
+                    </div>
                 </div>
-            </div>
-
-        <?php endforeach ?>
-    </div>
+            <?php endforeach ?>
+        </div>
+    </main>
 
 <?php
     include_once('templates/footer.php');
